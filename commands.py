cb152814aaf22ee0c37d2d4cb2b147c7a6c41a7d
Niko the mouse <75561812+nikothemouse@users.noreply.github.com>
2020-12-29 14:42:49 +1000

    Added other functions

--- commands.py
+++ commands.py
@@ -4,3 +4,21 @@ def w():
 # Пробел между строками
 def sp():
 	print('')
+def cd():
+	print('3')
+	w()
+	w()
+	sp()
+	w()
+	print('2')
+	w()
+	w()
+	sp()
+	w()
+	print('1')
+	w()
+	sp()
+	print('Бой!')
+	w()
+	w()
+	sp()
