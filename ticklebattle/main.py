93da78e0813424bdae894a38464c54efa282621c
Niko the mouse <75561812+nikothemouse@users.noreply.github.com>
2021-01-03 12:33:42 +1000

    Update main.py

--- ticklebattle/main.py
+++ ticklebattle/main.py
@@ -18,7 +18,7 @@ y1 = open(r'src/yaliya-techs.txt', 'r+')
 
 gold = open(r'src/golds.txt', 'r+')
 
-	# Читаем файлы имения тех или иных техник
+# Читаем файлы имения тех или иных техник
 g = gold.readline()
 g = int(g)
 v = v1.read(1)
@@ -163,7 +163,7 @@ def shop():
 
 
 # Покупка товара
-def purchace(techname):
+-def purchace(techname):
 	sp()
 	# Вампир
 	if techname == '1':
@@ -729,6 +729,7 @@ def purchace(techname):
 				gold.write(goldele)
 				j1.seek(0)
 				j = j1.write('1')
+
 			else:
 				shop()
 		else:
@@ -854,8 +855,15 @@ techs = ['Вампир', 'Феникс', 'Истребитель', 'Оружей
 coin = ['1','2']
 
 tf = choice(techs)
+def ct():
+	cointhrow = choice(coin)
+	if cointhrow == coin[0]:
+		print('Вы ходите первым B)!')
+		firstt = 1
+	elif cointhrow == coin[1]:
+		print('Вы ходите вторым :(!')
 
-def arith():
+def ch_op():
 	opponenta = choice(techs)
 	sp()
 	w()
@@ -863,96 +871,16 @@ def arith():
 	w()
 	cd()
 	print('Ваш протиник -', opponenta)
-	cointhrow = choice(coin)
-	if opponenta == techs[0]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
-	if opponenta == techs[1]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
-	if opponenta == techs[2]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
-	if opponenta == techs[3]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
-	if opponenta == techs[4]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
-	if opponenta == techs[5]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
-	if opponenta == techs[6]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
-	if opponenta == techs[7]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
-	if opponenta == techs[8]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
-	if opponenta == techs[9]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
-	if opponenta == techs[10]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
-	if opponenta == techs[11]:
-		if cointhrow == coin[0]:
-			print('Вы ходите первым B)!')
-			firstt = 1
-		elif cointhrow == coin[1]:
-			print('Вы ходите вторым :(!')
-			firstt = 2
+
+def arith():
+	cd()
+	ch_op()
+	ct()
 
 # Бой
 def fight():
 	while True:
-		fch = input('Какой режим выберем? Арифметический (A), геометричемкий (G) или пойдём в рейд на босса (R)?')
+		print('Отправимся в арифметический бой (A), в геометричесский (G) или пойдём в рейд на босса (R)?')
 		if fch == 'a' or fch == 'a' or fch == 'a' or fch == 'a':
 				arith()
 				break
@@ -963,6 +891,6 @@ def fight():
 				bossraid()
 				break
 		else:
-			print('НепрОвильный симвАл!')
+			print('Ниправельный симвал!!! [тут был Майндер)]')
 
-start()
+shop()
